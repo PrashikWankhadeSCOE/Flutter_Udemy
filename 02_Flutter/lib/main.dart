61705f4b86ccb7075ca35efc9403b5ce96344943
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2023-12-03 22:06:41 +0530

    button added

--- 02_Flutter/lib/main.dart
+++ 02_Flutter/lib/main.dart
@@ -7,15 +7,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      appBar: AppBar(title: const Text('Hello users !')),
-      body: const Center(
-        child: Text("Hello World !"),
-      ),
-    ));
+          appBar: AppBar(title: const Text('Hello users !')),
+          body: const Center(
+            child: Text("Hello World !"),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => debugPrint('Clicked'),
+            tooltip: 'jast nko dabu mobile futan ',
+            child: const Icon(Icons.account_circle),
+          ),
+        ));
   }
 }
