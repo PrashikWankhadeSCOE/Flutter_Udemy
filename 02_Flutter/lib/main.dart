7dee86ce9d6f2b717eeb3c47f0b279627c9394f4
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2023-12-04 22:30:38 +0530

    Tooltip things

--- 02_Flutter/lib/main.dart
+++ 02_Flutter/lib/main.dart
@@ -15,13 +15,34 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(title: const Text('Hello users !')),
           body: const Center(
-            child: Text("Hello World !"),
+            child: TooltipSample(),
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () => debugPrint('Clicked'),
-            tooltip: 'jast nko dabu mobile futan ',
+            tooltip: 'jast nko dabu tutan button ',
             child: const Icon(Icons.account_circle),
           ),
         ));
+  }
+}
+
+class TooltipSample extends StatelessWidget {
+  const TooltipSample({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Tooltip(
+      richMessage: TextSpan(
+        text: 'I am a rich tooltip how am i looking ',
+        style: TextStyle(color: Colors.red),
+        children: <InlineSpan>[
+          TextSpan(
+            text:
+                'I am another span of this rich tooltip and i am different ie bold',
+            style: TextStyle(fontWeight: FontWeight.w900),
+          ),
+        ],
+      ),
+      child: Text('tap and hold this text to see tooltip'),
+    );
   }
 }
