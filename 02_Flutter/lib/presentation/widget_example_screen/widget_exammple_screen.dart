63bbc4f67026c51807bebc85a7c20cd1893d8ce0
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2023-12-18 18:31:29 +0530

    Formating the complete project

--- 02_Flutter/lib/presentation/widget_example_screen/widget_exammple_screen.dart
+++ 02_Flutter/lib/presentation/widget_example_screen/widget_exammple_screen.dart
@@ -24,96 +24,94 @@ class WidgetExampleScreen extends StatelessWidget {
           fontStyle: FontStyle.italic,
         ),
       ))),
-      body: Container(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              const RowExpandedExample(),
-              const SizedBox(
-                height: 20,
-              ),
-              const FirstColumnChild(),
-              const SizedBox(
-                height: 20,
-              ),
-              const HelloWorldWidget(),
-              const SizedBox(
-                height: 20,
-              ),
-              const Person(
-                  picUrl:
-                      'https://www.shutterstock.com/shutterstock/photos/1218518830/display_1500/stock-photo-couple-of-sheep-run-across-the-mountain-meadow-cloudy-autumn-weather-1218518830.jpg',
-                  age: '22',
-                  name: 'Prashik',
-                  country: 'India',
-                  job: 'Unemployeed'),
-              const SizedBox(
-                height: 20,
-              ),
-              const Person(
-                  picUrl:
-                      'https://www.shutterstock.com/shutterstock/photos/159086927/display_1500/stock-photo-black-rowan-berries-on-branches-with-red-leaves-on-an-abstract-background-of-autumn-159086927.jpg',
-                  age: '10',
-                  name: 'Kabir',
-                  country: 'India',
-                  job: '5th Standard'),
-              const SizedBox(
-                height: 20,
-              ),
-              const NetworkImageExample(),
-              const SizedBox(
-                height: 20,
-              ),
-              const MediaQueryExample(),
-              const SizedBox(
-                height: 40,
-              ),
-              const LayoutBuilderExample(),
-              const SizedBox(
-                height: 40,
-              ),
-              const SnackBarExample(),
-              const SizedBox(
-                height: 40,
-              ),
-              const ButtonExample(),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomButton(
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    '/screenOne',
-                  );
-                },
-                icon: Icons.home,
-                iconColor: Colors.white,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CostumButtonGesture(
-                text: 'GestureButton',
-                onTap: () {
-                  // debugPrint('hello tapped');
-                  Navigator.pushNamed(
-                    context,
-                    '/screenTwo',
-                  );
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-            ],
-          ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const RowExpandedExample(),
+            const SizedBox(
+              height: 20,
+            ),
+            const FirstColumnChild(),
+            const SizedBox(
+              height: 20,
+            ),
+            const HelloWorldWidget(),
+            const SizedBox(
+              height: 20,
+            ),
+            const Person(
+                picUrl:
+                    'https://www.shutterstock.com/shutterstock/photos/1218518830/display_1500/stock-photo-couple-of-sheep-run-across-the-mountain-meadow-cloudy-autumn-weather-1218518830.jpg',
+                age: '22',
+                name: 'Prashik',
+                country: 'India',
+                job: 'Unemployeed'),
+            const SizedBox(
+              height: 20,
+            ),
+            const Person(
+                picUrl:
+                    'https://www.shutterstock.com/shutterstock/photos/159086927/display_1500/stock-photo-black-rowan-berries-on-branches-with-red-leaves-on-an-abstract-background-of-autumn-159086927.jpg',
+                age: '10',
+                name: 'Kabir',
+                country: 'India',
+                job: '5th Standard'),
+            const SizedBox(
+              height: 20,
+            ),
+            const NetworkImageExample(),
+            const SizedBox(
+              height: 20,
+            ),
+            const MediaQueryExample(),
+            const SizedBox(
+              height: 40,
+            ),
+            const LayoutBuilderExample(),
+            const SizedBox(
+              height: 40,
+            ),
+            const SnackBarExample(),
+            const SizedBox(
+              height: 40,
+            ),
+            const ButtonExample(),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomButton(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  '/screenOne',
+                );
+              },
+              icon: Icons.home,
+              iconColor: Colors.white,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CostumButtonGesture(
+              text: 'GestureButton',
+              onTap: () {
+                // debugPrint('hello tapped');
+                Navigator.pushNamed(
+                  context,
+                  '/screenTwo',
+                );
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
