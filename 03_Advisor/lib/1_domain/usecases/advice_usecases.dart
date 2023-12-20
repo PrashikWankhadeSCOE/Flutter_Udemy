a782e425770ae409a539d6592c2f2f0691839d5d
PrashikWankhadeSCOE <prashikwankhade68@gmail.com>
2023-12-20 17:08:12 +0530

    Data Layers -- Models and Datasources

--- 03_Advisor/lib/1_domain/usecases/advice_usecases.dart
+++ 03_Advisor/lib/1_domain/usecases/advice_usecases.dart
@@ -1,18 +1,12 @@
+import 'package:advisor/0_data/repository/advice_repo_impl.dart';
 import 'package:advisor/1_domain/entities/advice_entities.dart';
 import 'package:advisor/1_domain/failures/advice_failure.dart';
 import 'package:dartz/dartz.dart';
 
 class AdviceUsecases {
+  final adviceRepo = AdviceRepoImpl();
   Future<Either<Failure, AdviceEntity>> getAdvice() async {
-    // call a repository to get a data (failure or data)
-    // for example get an advice
-    await Future.delayed(const Duration(seconds: 3), () => {});
-    // Call to repo was successfull : -> return data not failure
-    //return right(const AdviceEntity(
-    //    advice:
-    //        'We cannot solve problems with the kind of thinking we employed when we came up with them.” — Albert Einstein',
-    //    id: 1));
-    //Call to repo went wrong and returning failure
-    return left(CacheFailure());
+    return adviceRepo.getAdviceFromDatasource();
+    //! Space for bussiness Logic
   }
 }
