d575bb4b94b65c2e6e4d59a104c822a9b363c05d
Flutter-Prashik <flutter.prashik@gmail.com>
2024-01-16 13:16:55 +0530

    Unit Testing - Buisness logic (bloc/cubit)

--- 03_Advisor/lib/3_application/pages/advice/cubit/advicer_state.dart
+++ 03_Advisor/lib/3_application/pages/advice/cubit/advicer_state.dart
@@ -2,24 +2,29 @@ part of 'advicer_cubit.dart';
 
 @immutable
 sealed class AdvicerCubitState extends Equatable {
+  const AdvicerCubitState();
   @override
   List<Object?> get props => [];
 }
 
-class AdvicerInitial extends AdvicerCubitState {}
+class AdvicerInitial extends AdvicerCubitState {
+  const AdvicerInitial();
+}
 
-class AdvicerStateLoading extends AdvicerCubitState {}
+class AdvicerStateLoading extends AdvicerCubitState {
+  const AdvicerStateLoading();
+}
 
 class AdvicerStateLoaded extends AdvicerCubitState {
   final String advice;
-  AdvicerStateLoaded({required this.advice});
+  const AdvicerStateLoaded({required this.advice});
   @override
   List<Object?> get props => [advice];
 }
 
 class AdvicerStateError extends AdvicerCubitState {
   final String message;
-  AdvicerStateError({required this.message});
+  const AdvicerStateError({required this.message});
   @override
   List<Object?> get props => [message];
 }
